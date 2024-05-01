bcf1579bbdaa1d7992a39ef504cba256c3c7a403
Manikandan Nishanth M <133948661+ManikandanNishanth1605@users.noreply.github.com>
2024-05-01 16:22:25 +0530

    Created new repo and files are added

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new typed(".typed",{
+    Strings: ["3D printing enthusiast", "Mechanical Engineering student"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
